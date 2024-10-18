00443ef0b24d11b9b1923b858a868c0813f92e23
Shone <shonegr28@gmail.com>
2024-10-18 12:14:49 -0500

    Pushingall

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+
+        <div className='hero-text'>
+
+          
+
+            <h2>Find Your Spot Faster</h2>
+            
+
+            
+            
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
